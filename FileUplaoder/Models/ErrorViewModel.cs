55a8b6a1efde1856a9f9959cc272801cbd3e8646
Lamyaa Q <lamiaa@Lamyaas-MacBook-Pro.local>
2022-09-26 12:16:34 +0300

    initial commit: project setup

---
+++ FileUplaoder/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace FileUplaoder.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
